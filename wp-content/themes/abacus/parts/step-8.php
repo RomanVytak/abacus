686dbf7c5c56d76422072bbad389e8cf6a163779
Roman Vytak <r.vytak@dynatech.lv>
2025-07-06 16:58:07 +0300

    updates

--- wp-content/themes/abacus/parts/step-8.php
+++ wp-content/themes/abacus/parts/step-8.php
@@ -6,11 +6,12 @@
       <div class="items">
         <?php while (have_rows('reviews')) : the_row();
           $video = substr(get_sub_field('reviews_video'), strpos(get_sub_field('reviews_video'), "=") + 1);
+          $custom_preview = get_sub_field('video_preview');
         ?>
           <div class="item">
             <div class="video">
               <div class="img" data-id="<?php echo $video; ?>">
-                <img src="https://img.youtube.com/vi/<?php echo $video; ?>/hqdefault.jpg" alt="video">
+                <img src="<?php echo $custom_preview ? esc_url($custom_preview) : "https://img.youtube.com/vi/$video/hqdefault.jpg"; ?>" alt="video">
                 <span class="play"></span>
               </div>
             </div>
